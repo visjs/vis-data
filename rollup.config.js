298c49d76b13f7d4c2adee0aa22909c04f81fd0d
Tomáš Vyčítal <tom.vycital@gmail.com>
2019-12-30 20:18:01 +0100

    fix(build): use common header and Babel preset (#54)
    See visjs/vis-dev-utils#5.
    
    The build in banner was used instead of the plugin (the plugin was
    messing up source maps which now work).

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,7 @@ import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import typescript from 'rollup-plugin-typescript2'
-import banner from 'rollup-plugin-banner'
-import genHeader from './lib/header'
+import { generateHeader } from 'vis-dev-utils'
 
 // TypeScript because Babel transpiles modules in isolation, therefore no type reexports.
 // CommonJS because Babel is not 100 % ESM.
@@ -15,7 +14,7 @@ const babelConfingBase = {
 const resolveConfig = {
   extensions: [...babelConfingBase.extensions, '.json'],
 }
-const bannerConfig = genHeader('data')
+const banner = generateHeader()
 const typescriptConfig = {
   tsconfig: 'tsconfig.code.json',
 }
@@ -24,32 +23,34 @@ export default [
   {
     input: 'src/index.ts',
     output: {
+      banner,
       file: 'dist/esm.js',
       format: 'esm',
+      sourcemap: true,
     },
     plugins: [
       resolve(resolveConfig),
       typescript(typescriptConfig),
       commonjs(),
       babel(babelConfingBase),
-      banner(bannerConfig),
     ],
   },
   {
     input: 'src/index.ts',
     output: {
+      banner,
       file: 'dist/umd.js',
       format: 'umd',
       exports: 'named',
       name: 'vis',
-      extend: true
+      extend: true,
+      sourcemap: true,
     },
     plugins: [
       resolve(resolveConfig),
       typescript(typescriptConfig),
       commonjs(),
       babel(babelConfingBase),
-      banner(bannerConfig),
     ],
   },
 ]
