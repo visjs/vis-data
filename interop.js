335cecc337ebd9018c9887052ff23c5a0aa75a01
Tomáš Vyčítal <tom.vycital@gmail.com>
2020-08-09 10:37:03 +0200

    test: add E2E interop testing (#401)

---
+++ interop.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+// Ideally this would be just a Shell script but Windows exists and I don't want
+// to force Windows users to install Shell and configure it to be used to run
+// npm scripts (and break their Windows only projects?).
+
+const { spawnSync } = require("child_process");
+
+const { status } = spawnSync(
+  "npx",
+  [
+    "test-e2e-interop",
+    // This is the shared stuff used in all of our projects.
+    "--config",
+    "./node_modules/vis-dev-utils/dist/interop/base-config.json",
+    // This is specific to this project.
+    "--project",
+    "vis-charts https://github.com/visjs/vis-charts.git",
+    "--project",
+    "vis-data ./vis-data-0.0.0-no-version.tgz",
+    "--project",
+    "vis-graph3d https://github.com/visjs/vis-graph3d.git",
+    "--project",
+    "vis-network https://github.com/visjs/vis-network.git",
+    "--project",
+    "vis-timeline https://github.com/visjs/vis-timeline.git",
+    // Any additional options passed from the command line (e.g. a fail command
+    // for debugging).
+    ...process.argv.slice(2),
+  ],
+  { stdio: "inherit" }
+);
+process.exitCode = status;
